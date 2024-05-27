5d16d777f5b4aa8bf6dee06b1085c71bf50875fc
AryaShakya1 <arya@ramailo.tech>
2024-05-27 21:13:36 +0545

    Show time when weather was updated

--- frontend/src/components/WeatherCard.tsx
+++ frontend/src/components/WeatherCard.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
 
 function WeatherCard({ weatherData }: { weatherData: WeatherType }) {
+    const lastUpdated = new Date(weatherData.last_updated).toLocaleString()
     return (
         <div className='flex flex-col bg-blue-200 rounded-xl p-4 m-2'>
             <p className='text-4xl p-2 font-bold'>{weatherData.location.name}, {weatherData.location.country}</p>
@@ -13,6 +13,8 @@ function WeatherCard({ weatherData }: { weatherData: WeatherType }) {
             <p className='text-xl p-2'>Wind: {weatherData.wind_kph} kph / {weatherData.wind_mph} mph       {weatherData.wind_degree} {weatherData.wind_dir}</p>
             <p className='text-xl p-2'>Wind: {weatherData.wind_kph} kph / {weatherData.wind_mph} mph       {weatherData.wind_degree} {weatherData.wind_dir}</p>
             <p className='text-xl p-2'>UV: {weatherData.uv} </p>
+            <div className="mt-4"></div>
+            <p className='text-sm p-2 text-gray-700'>Updated at: {lastUpdated}</p>
         </div>
 
     )
