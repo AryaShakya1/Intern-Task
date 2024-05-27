fbe3cfd9077517b95f7534172fbf260f6ee609dc
AryaShakya1 <arya@ramailo.tech>
2024-05-27 21:44:45 +0545

    Add functionality to refresh the weather

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -39,9 +39,38 @@ function HomePage() {
         }
     }
 
+    async function refresh_weather_data(location: string) {
+        try {
+            setErrorMessage('')
+            if (location.length == 0) {
+                setErrorMessage("Location cannot be Empty.")
+                return
+            }
+            if (!navigator.onLine) {
+                setIsConnected(false)
+            }
+            else { setIsConnected(true) }
+            const response = await fetch(baseUrl + 'fetch/', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ location: location })
+            })
+            if (response.ok) {
+                const data = await response.json()
+                setWeatherData(data)
+                setIsLoaded(true)
+            }
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+
 
     return (
-        <div className='flex flex-col items-center justify-evenly p-4 bg-slate-300 min-h-screen'>
+        <div className='flex flex-col items-center justify-evenly p-4 bg-gradient-to-tr from-cyan-300 to-sky-600 min-h-screen'>
             <p className='text-black text-5xl font-extrabold'>Weather Data</p>
             <div className="flex flex-row justify-center min-w-full">
                 <input type="text" name="location" id="location" placeholder='Enter location...' className='m-4 py-4 px-8 bg-gray-200 rounded-full focus:outline-none w-96' value={location} onChange={(e) => setLocation(e.target.value)} />
