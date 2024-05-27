3be7ac5dfeff9d3ff996bbd276f1d1403980e326
AryaShakya1 <arya@ramailo.tech>
2024-05-27 14:32:41 +0545

    Define types of Response object

---
+++ frontend/src/types.ts
@@ -0,0 +1,41 @@
+type LocationType = {
+  lat: number;
+  lon: number;
+  name: string;
+  region: string;
+  country: string;
+  tz_id: string;
+  localtime_epoch: number;
+  localtime: string;
+};
+
+type Condition = {
+  text: string;
+  icon: string;
+  code: number;
+};
+
+type WeatherType = {
+  location: LocationType;
+  last_updated: Date;
+  last_updated_epoch: number;
+  temp_c: number;
+  temp_f: number;
+  feelslike_c: number;
+  feelslike_f: number;
+  condition: Condition;
+  wind_mph: number;
+  wind_kph: number;
+  wind_degree: number;
+  wind_dir: string;
+  pressure_mb: number;
+  pressure_in: number;
+  precip_mm: number;
+  precip_in: number;
+  humidity: number;
+  cloud: number;
+  is_day: number;
+  uv: number;
+  gust_mph: number;
+  gust_kph: number;
+};
