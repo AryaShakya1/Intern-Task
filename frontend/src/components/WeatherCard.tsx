abb0c561c829b8b903629f50691d16221b0f0fb6
AryaShakya1 <arya@ramailo.tech>
2024-05-27 22:01:41 +0545

    Convert date into human readable format

--- frontend/src/components/WeatherCard.tsx
+++ frontend/src/components/WeatherCard.tsx
@@ -1,6 +1,54 @@
+function timeAgo(date: Date) {
+    const seconds = Math.floor((new Date().valueOf() - date.valueOf()) / 1000);
+
+    const interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+        return interval + " years ago";
+    }
+    if (interval === 1) {
+        return interval + " year ago";
+    }
+
+    const months = Math.floor(seconds / 2628000);
+    if (months > 1) {
+        return months + " months ago";
+    }
+    if (months === 1) {
+        return months + " month ago";
+    }
+
+    const days = Math.floor(seconds / 86400);
+    if (days > 1) {
+        return days + " days ago";
+    }
+    if (days === 1) {
+        return days + " day ago";
+    }
+
+    const hours = Math.floor(seconds / 3600);
+    if (hours > 1) {
+        return hours + " hours ago";
+    }
+    if (hours === 1) {
+        return hours + " hour ago";
+    }
+
+    const minutes = Math.floor(seconds / 60);
+    if (minutes > 1) {
+        return minutes + " minutes ago";
+    }
+    if (minutes === 1) {
+        return minutes + " minute ago";
+    }
+
+    return "just now";
+}
+
 
 function WeatherCard({ weatherData }: { weatherData: WeatherType }) {
-    const lastUpdated = new Date(weatherData.last_updated).toLocaleString()
+    const lastUpdatedDate = new Date(weatherData.last_updated)
+    const lastUpdated = timeAgo(lastUpdatedDate)
     return (
         <div className='flex flex-col bg-blue-200 rounded-xl p-4 m-2'>
             <p className='text-4xl p-2 font-bold'>{weatherData.location.name}, {weatherData.location.country}</p>
