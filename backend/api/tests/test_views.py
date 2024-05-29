b79e200a724fccdc0959b8babc06cc3af17bc8dc
AryaShakya1 <arya@ramailo.tech>
2024-05-29 12:59:41 +0545

    Set up dummy data for testing

--- backend/api/tests/test_views.py
+++ backend/api/tests/test_views.py
@@ -1,9 +1,48 @@
+import datetime
 from django.test import TestCase
 
-from api.models import Location
+from api.models import Location, RealTimeWeather
+from api.serializers import RealTimeWeatherSerializer
 
 
 class GetWeatherViewTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.location = Location.objects.create(
+            lat=27.72,
+            lon=85.32,
+            name="Kathmandu",
+            region="",
+            country="Nepal",
+            tz_id="Asia/Kathmandu",
+            localtime_epoch=1716884072,
+            localtime="2024-05-28 13:59",
+        )
+        cls.weather = RealTimeWeather.objects.create(
+            location=cls.location,
+            last_updated=datetime.datetime.now(),
+            last_updated_epoch=1716884072,
+            temp_c=29.66,
+            temp_f=98.44,
+            feelslike_c=28.56,
+            feelslike_f=97.23,
+            condition={},
+            wind_mph=15.49,
+            wind_kph=15.49,
+            wind_degree=21,
+            wind_dir="NW",
+            pressure_mb=15.49,
+            pressure_in=15.49,
+            precip_mm=15.49,
+            precip_in=15.49,
+            humidity=19,
+            cloud=20,
+            is_day=1,
+            uv=15.49,
+            gust_mph=15.49,
+            gust_kph=15.49,
+        )
 
     def test_view_does_not_allow_get_request(self):
         response = self.client.get("/api/")
